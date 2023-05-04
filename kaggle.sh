2c3af620ca1f5922bed5aba38c8e0c57750928ed
Paul T <pault-tech@github.com>
2023-05-04 22:01:24 +0000

    Wip

---
+++ kaggle.sh
@@ -0,0 +1,8 @@
+mkdir input
+
+cd input
+kaggle competitions download -c amp-parkinsons-disease-progression-prediction
+
+# kaggle datasets list
+
+kaggle kernels pull gusthema/parkinson-s-disease-progression-prediction-w-tfdf
