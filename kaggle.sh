a591d458d9405f3d69f433f90669d1fb9e4f978e
Paul T <pault-tech@github.com>
2023-05-05 00:10:28 +0000

    updat

--- kaggle.sh
+++ kaggle.sh
@@ -6,4 +6,17 @@ unzip amp-parkinsons-disease-progression-prediction
 # kaggle datasets list
 
 cd ..
+
 kaggle kernels pull gusthema/parkinson-s-disease-progression-prediction-w-tfdf
+
+# pip install --user tensorfljow (throws killed error, mb oom?)
+# pip install --user tensorflow --no-cache-dir
+pip install --user tensorflow_decision_forests --no-cache-dir
+
+
+whoami
+pwd
+sudo mkdir /kaggle
+sudo chown codespace /kaggle
+ln -s /workspace/kaggle-parkinsons/input /kaggle/
+ls /kaggle/input/
